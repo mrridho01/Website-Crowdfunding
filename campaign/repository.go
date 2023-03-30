68239b1742b19c3555dfa731ce9c82d85be17ca9
mrridho01 <muhammadridho01@gmail.com>
2023-03-30 20:08:56 +0700

    preload images findall campaign

--- campaign/repository.go
+++ campaign/repository.go
@@ -19,7 +19,7 @@ func NewRepository(db *gorm.DB) *repository {
 // repo untuk get semua campaign tanpa filter
 func (r *repository) FindAll() ([]Campaign, error) {
 	var campaigns []Campaign
-	err := r.db.Find(&campaigns).Error
+	err := r.db.Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
 	if err != nil {
 		return campaigns, err
 	}
