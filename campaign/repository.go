96c746bdae6690af5fa6e68794f9973a778d1c17
mrridho01 <muhammadridho01@gmail.com>
2023-03-30 19:37:33 +0700

    repository campaign, find all and by userid

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,38 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	FindAll() ([]Campaign, error)
+	FindByUserId(userId uint) ([]Campaign, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+// generate instance struct repository agar bisa diakses dari package luar
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+// repo untuk get semua campaign tanpa filter
+func (r *repository) FindAll() ([]Campaign, error) {
+	var campaigns []Campaign
+	err := r.db.Find(&campaigns).Error
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
+
+func (r *repository) FindByUSerId(userID uint) ([]Campaign, error) {
+	var campaigns []Campaign
+	err := r.db.Where("user_id = ?", userID).Find(&campaigns).Error
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
