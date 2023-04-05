0e0b14998005de0a3b8e641db5b3273f8a249053
mrridho01 <muhammadridho01@gmail.com>
2023-04-06 04:58:08 +0700

    create campaign service

--- campaign/service.go
+++ campaign/service.go
@@ -1,8 +1,15 @@
 package campaign
 
+import (
+	"fmt"
+
+	"github.com/gosimple/slug"
+)
+
 type Service interface {
 	GetCampaigns(userId uint) ([]Campaign, error)
 	GetCampaignById(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -40,4 +47,27 @@ func (s *service) GetCampaignById(input GetCampaignDetailInput) (Campaign, error
 	}
 
 	return campaign, nil
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{
+		Name:             input.Name,
+		ShortDescription: input.ShortDescription,
+		Description:      input.Description,
+		Perks:            input.Perks,
+		GoalAmount:       input.GoalAmount,
+		UserId:           input.User.ID,
+	}
+
+	// generate slug
+	slugName := fmt.Sprintf("%s %d", input.Name, input.User.ID)
+	campaign.Slug = slug.Make(slugName)
+
+	newCampaign, err := s.repository.Save(campaign)
+	if err != nil {
+		return newCampaign, err
+	}
+
+	return newCampaign, nil
+
 }
