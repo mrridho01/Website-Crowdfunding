2368f51bd982c3bfef93c572fbdc9ac9b7cd0ed2
mrridho01 <muhammadridho01@gmail.com>
2023-04-06 19:51:08 +0700

    create service update campaign

--- campaign/service.go
+++ campaign/service.go
@@ -10,6 +10,7 @@ type Service interface {
 	GetCampaigns(userId uint) ([]Campaign, error)
 	GetCampaignById(input GetCampaignDetailInput) (Campaign, error)
 	CreateCampaign(input CreateCampaignInput) (Campaign, error)
+	UpdateCampaign(id GetCampaignDetailInput, inputData CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -70,4 +71,25 @@ func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
 
 	return newCampaign, nil
 
+}
+
+func (s *service) UpdateCampaign(id GetCampaignDetailInput, inputData CreateCampaignInput) (Campaign, error) {
+	campaign, err := s.repository.FindById(id.Id)
+	if err != nil {
+		return campaign, err
+	}
+
+	//update data campaign dari inputData
+	campaign.Name = inputData.Name
+	campaign.ShortDescription = inputData.ShortDescription
+	campaign.Description = inputData.Description
+	campaign.Perks = inputData.Perks
+	campaign.GoalAmount = inputData.GoalAmount
+
+	updatedCampaign, err := s.repository.Update(campaign)
+	if err != nil {
+		return updatedCampaign, err
+	}
+
+	return updatedCampaign, nil
 }
