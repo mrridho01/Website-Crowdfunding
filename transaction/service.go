c82eede2a3961f387a788b5d7e2a466fc7437e14
mrridho01 <muhammadridho01@gmail.com>
2023-04-23 20:57:49 +0700

    service get transaction by campaign id

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,23 @@
 package transaction
+
+type Service interface {
+	GetTransactionsByCampaignId(campaignId uint) ([]Transaction, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionsByCampaignId(campaignId uint) ([]Transaction, error) {
+	transactions, err := s.repository.FindByCampaignId(campaignId)
+
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
