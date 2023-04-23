5d38d60cea9000f2ebbd4f1525d0aeb4f121951c
mrridho01 <muhammadridho01@gmail.com>
2023-04-23 20:52:08 +0700

    repo get transaction by campaign id

--- transaction/repository.go
+++ transaction/repository.go
@@ -2,8 +2,26 @@ package transaction
 
 import "gorm.io/gorm"
 
-type Repository interface{}
+type Repository interface {
+	FindByCampaignId(campaignId uint) ([]Transaction, error)
+}
 
 type repository struct {
 	db *gorm.DB
+}
+
+// generate instance struct repository agar bisa diakses dari package luar
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) FindByCampaignId(campaignId uint) ([]Transaction, error) {
+	var transaction []Transaction
+	err := r.db.Where("campaign_id = ?", campaignId).Preload("User").Find(&transaction).Error
+
+	if err != nil {
+		return transaction, err
+	}
+
+	return transaction, nil
 }
